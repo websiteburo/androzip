9435074c584b59c9823068a636ce4554994253a0
Henri Bourcereau <henri@websiteburo.com>
2011-04-26 13:18:57 +0200

    first commit

---
+++ example/app.js
@@ -0,0 +1,25 @@
+//app.js: download zip from DropBox to Documents folder
+//        and extact file to Documents folder
+ 
+Titanium.UI.setBackgroundColor('#000');
+ 
+var win1 = Titanium.UI.createWindow({
+      title:'AndroZip test',
+      backgroundColor:'#000'
+});
+ 
+win1.open();
+var xhr = Titanium.Network.createHTTPClient();
+
+var androzip = require('com.websiteburo.androzip');
+Ti.API.info("module is => " + androzip);
+
+ 
+xhr.onload = function(){
+   var f = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'mapimages.6.zip');
+   var fdir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory);
+   f.write(this.responseData);
+   androzip.extract(f.nativePath, fdir.nativePath);
+};
+xhr.open('GET','http://dl.dropbox.com/u/1400234/test.zip');
+xhr.send();
---
+++ src/com/websiteburo/androzip/AndrozipModule.java
@@ -0,0 +1,83 @@
+package com.websiteburo.androzip;
+
+import java.io.*;
+import java.util.zip.*;
+
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.annotations.Kroll;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.util.Log;
+import org.appcelerator.titanium.util.TiConfig;
+
+@Kroll.module(name="Androzip", id="com.websiteburo.androzip")
+public class AndrozipModule extends KrollModule
+{
+
+	// Standard Debugging variables
+	private static final String LCAT = "AndrozipModule";
+	private static final boolean DBG = TiConfig.LOGD;
+	
+	public AndrozipModule(TiContext tiContext) {
+		super(tiContext);
+	}
+
+	// Methods
+	@Kroll.method
+	public void extract(String sourcefile, String dirout) throws IOException {
+		int plen = "file://".length();
+		sourcefile = sourcefile.substring(plen);
+		dirout = dirout.substring(plen);
+		if(!sourcefile.endsWith(".zip")){
+			Log.d(LCAT, "Invalid file name!");
+	    }
+	    else if(!new File(sourcefile).exists()){
+	    	Log.d(LCAT, "File not exist : " + sourcefile + "!");
+	    }
+	    else {
+	    	ZipInputStream in = new ZipInputStream(new FileInputStream(sourcefile));
+	    	ZipEntry ze = null;
+	    	byte[] buf = new byte[8096];
+	    	int a = 0;
+	      
+	    	while((ze = in.getNextEntry()) != null) {
+	    		String name = ze.getName();
+		    	if(name.length() > 0) {
+					if (DBG) {
+						Log.d(LCAT, "Extracting " + name);
+					}
+					if (ze.isDirectory()) {
+						File d = new File(dirout, name);
+						d.mkdirs();
+						if (DBG) {
+							Log.d(LCAT, "Created directory " + d.toString());
+						}
+						d = null;
+					} else {
+						FileOutputStream fos = null;
+						try {
+							fos = new FileOutputStream(new File(dirout,name));
+							int read = 0;
+							while((read = in.read(buf)) != -1) {
+								fos.write(buf, 0, read);
+							}
+							a = a + 1;
+						} finally {
+							if (fos != null) {
+								try {
+									fos.close();
+								} catch (Throwable t) {
+									//Ignore
+								}
+							}
+						}
+					}
+		    	}
+		    	in.closeEntry();
+	      }
+	      
+	      if(a > 0) Log.d(LCAT, Integer.toString(a) +  "Files unzipped.");
+	      in.close();
+	    }
+	}
+}
